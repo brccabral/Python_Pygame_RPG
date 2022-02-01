0b2d128ce195498c9d9f21d1c4ad2ecca60ba5cb
Bruno Cabral <cabral.brc@gmail.com>
2022-01-31 23:26:40 -0800

    level prints maps positions

--- level.py
+++ level.py
@@ -1,15 +1,23 @@
 import pygame
+from settings import *
 
 class Level:
     def __init__(self):
 
         # get the display surface
         self.display_surface = pygame.display.get_surface()
-        
+
         # sprite group setup
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
+
+        self.create_map()
     
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for column_index, column in enumerate(row):
+                print(f"{row_index} {column_index}")
+
     def run(self):
         # update and draw the level
         pass
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import imp
 import pygame, sys
 from settings import *
 from level import Level
