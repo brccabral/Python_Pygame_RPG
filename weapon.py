a0748409afd56119077476b845b8beadab30eb58
Bruno Cabral <cabral.brc@gmail.com>
2022-02-01 21:45:11 -0800

    show weapon in correct position

--- weapon.py
+++ weapon.py
@@ -4,8 +4,22 @@ from player import Player
 class Weapon(pygame.sprite.Sprite):
     def __init__(self, player: Player, groups):
         super().__init__(groups)
+        direction = player.status.split('_')[0]
+
+        # graphic
         self.image = pygame.Surface((40, 40))
-        self.rect = self.image.get_rect(center = player.rect.center)
+
+        # placement
+        if direction == 'right':
+            self.rect = self.image.get_rect(midleft = player.rect.midright + pygame.math.Vector2(0, 16))
+        elif direction == 'left':
+            self.rect = self.image.get_rect(midright = player.rect.midleft + pygame.math.Vector2(0, 16))
+        elif direction == 'down':
+            self.rect = self.image.get_rect(midtop = player.rect.midbottom + pygame.math.Vector2(-10, 0))
+        elif direction == 'up':
+            self.rect = self.image.get_rect(midbottom = player.rect.midtop + pygame.math.Vector2(-10, 0))
+        else:
+            self.rect = self.image.get_rect(center = player.rect.center)
 
 if __name__ == '__main__':
     from main import run_game
