b7268a9a1d3646773efb773d9fe2910904121f70
Bruno Cabral <cabral.brc@gmail.com>
2022-02-01 20:23:06 -0800

    attack and magic as inputs

--- player.py
+++ player.py
@@ -9,14 +9,19 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0, -26) # same center, but smaller Y size
 
+        # moviment
         self.direction = pygame.math.Vector2()
         self.speed = 5
+        self.attacking = False
+        self.attack_cooldown = 400
+        self.attack_time = None
 
         self.obstacles_sprites = obstacles_sprites
     
     def input(self):
         keys = pygame.key.get_pressed()
 
+        # move input
         if keys[pygame.K_UP]:
             self.direction.y = -1
         elif keys[pygame.K_DOWN]:
@@ -30,6 +35,24 @@ class Player(pygame.sprite.Sprite):
             self.direction.x = 1
         else:
             self.direction.x = 0
+        
+        # attack input
+        if keys[pygame.K_SPACE] and not self.attacking:
+            print('attack')
+            self.attacking = True
+            self.attack_time = pygame.time.get_ticks()
+        
+        # magic input
+        if keys[pygame.K_LCTRL] and not self.attacking:
+            print('magic')
+            self.attacking = True
+            self.attack_time = pygame.time.get_ticks()
+
+    def cooldowns(self):
+        current_time = pygame.time.get_ticks()
+        if self.attacking:
+            if current_time - self.attack_time >= self.attack_cooldown:
+                self.attacking = False
     
     def move(self, speed):
         # limit player speed when going diagonal
@@ -65,6 +88,7 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         self.input()
         self.move(self.speed)
+        self.cooldowns()
 
 if __name__ == '__main__':
     from main import run_game
