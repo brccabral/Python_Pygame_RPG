cdcd3153314f85cb53cb07b5d3aded80e6a43e6e
Bruno Cabral <cabral.brc@gmail.com>
2022-02-01 23:20:05 -0800

    class entity init

---
+++ entity.py
@@ -0,0 +1,40 @@
+import pygame
+
+class Entity(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+    
+    def move(self, speed):
+        # limit player speed when going diagonal
+        # diagonal direction increases vector length
+        # use normalize to set vector size to 1
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+        
+        self.hitbox.x += self.direction.x * speed
+        self.collision('horizontal')
+        self.hitbox.y += self.direction.y * speed
+        self.collision('vertical')
+
+        self.rect.center = self.hitbox.center
+    
+    def collision(self, direction: str):
+        if direction == 'horizontal':
+            for sprite in self.obstacles_sprites:
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.x > 0: # moving right
+                        self.hitbox.right = sprite.hitbox.left # colliding from left to right
+                    elif self.direction.x < 0: # moving left
+                        self.hitbox.left = sprite.hitbox.right
+
+        if direction == 'vertical':
+            for sprite in self.obstacles_sprites:
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.y > 0: # moving right
+                        self.hitbox.bottom = sprite.hitbox.top # colliding from left to right
+                    elif self.direction.y < 0: # moving left
+                        self.hitbox.top = sprite.hitbox.bottom
+
+if __name__ == '__main__':
+    from main import run_game
+    run_game()
--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 from debug import debug
 from typing import Callable, Dict, List
 import pygame
+from entity import Entity
 from settings import *
 from os import walk
 
 from support import import_folder
 
-class Player(pygame.sprite.Sprite):
+class Player(Entity):
     def __init__(self, pos: tuple, groups: List[pygame.sprite.Group], 
                 obstacles_sprites: pygame.sprite.Group, 
                 create_attack: Callable, destroy_attack: Callable,
@@ -173,37 +174,6 @@ class Player(pygame.sprite.Sprite):
             if current_time - self.magic_switch_time >= self.switch_cooldown:
                 self.can_switch_magic = True
     
-    def move(self, speed):
-        # limit player speed when going diagonal
-        # diagonal direction increases vector length
-        # use normalize to set vector size to 1
-        if self.direction.magnitude() != 0:
-            self.direction = self.direction.normalize()
-        
-        self.hitbox.x += self.direction.x * speed
-        self.collision('horizontal')
-        self.hitbox.y += self.direction.y * speed
-        self.collision('vertical')
-
-        self.rect.center = self.hitbox.center
-    
-    def collision(self, direction: str):
-        if direction == 'horizontal':
-            for sprite in self.obstacles_sprites:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.x > 0: # moving right
-                        self.hitbox.right = sprite.hitbox.left # colliding from left to right
-                    elif self.direction.x < 0: # moving left
-                        self.hitbox.left = sprite.hitbox.right
-
-        if direction == 'vertical':
-            for sprite in self.obstacles_sprites:
-                if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.y > 0: # moving right
-                        self.hitbox.bottom = sprite.hitbox.top # colliding from left to right
-                    elif self.direction.y < 0: # moving left
-                        self.hitbox.top = sprite.hitbox.bottom
-
     def update(self):
         self.input()
         self.move(self.speed)
