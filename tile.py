507166769f1d2d4eead497fbe4bfc90e91c2812e
Bruno Cabral <cabral.brc@gmail.com>
2022-02-01 19:38:49 -0800

    import layout from csv

--- tile.py
+++ tile.py
@@ -3,8 +3,9 @@ import pygame
 from settings import *
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos: tuple, groups: List[pygame.sprite.Group]):
+    def __init__(self, pos: tuple, groups: List[pygame.sprite.Group], sprite_type, surface = pygame.Surface((TILESIZE, TILESIZE))):
         super().__init__(groups)
-        self.image = pygame.image.load('graphics/test/rock.png').convert_alpha()
+        self.sprite_type = sprite_type
+        self.image = surface
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0, -10) # same center, but smaller Y size
