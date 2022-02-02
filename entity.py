a227243a33655a0e64b2b7212c88da203eb678f4
Bruno Cabral <cabral.brc@gmail.com>
2022-02-02 12:27:12 -0800

    enemy flicker when hit

--- entity.py
+++ entity.py
@@ -1,4 +1,5 @@
 import pygame
+from math import sin
 
 class Entity(pygame.sprite.Sprite):
     def __init__(self, groups):
@@ -46,6 +47,11 @@ class Entity(pygame.sprite.Sprite):
                         self.hitbox.bottom = sprite.hitbox.top # colliding from left to right
                     elif self.direction.y < 0: # moving left
                         self.hitbox.top = sprite.hitbox.bottom
+
+    def wave_value(self):
+        value = sin(pygame.time.get_ticks())
+        if value >= 0: return 255
+        else: return 0
 
 if __name__ == '__main__':
     from main import run_game
