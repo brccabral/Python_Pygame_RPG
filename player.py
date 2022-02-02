9a5e888fec43968f1e2b79d44388b96511dff07e
Bruno Cabral <cabral.brc@gmail.com>
2022-02-01 23:04:43 -0800

    add player base magic

--- player.py
+++ player.py
@@ -133,7 +133,7 @@ class Player(pygame.sprite.Sprite):
             self.attacking = True
             self.attack_time = pygame.time.get_ticks()
             style = list(magic_data.keys())[self.magic_index]
-            strength = list(magic_data.values())[self.magic_index]['strength']
+            strength = list(magic_data.values())[self.magic_index]['strength'] + self.stats['magic']
             cost = list(magic_data.values())[self.magic_index]['cost']
             self.create_magic(style, strength, cost)
         
